196075d79b795c3e8ef3e947fcd37ccb7f3a95f0
Dubidapdap0798 <116095239+Dubidapdap0798@users.noreply.github.com>
2023-09-05 00:09:09 +0800

    32k

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ async def chat(ctx : discord.Message, *, text):
 
 
         response = await openai.ChatCompletion.acreate(
-            model="gpt-3.5",
+            model="gpt-4-32k",
             messages= messages,
             user = str(ctx.author.id)
     )
